db8e813b147e78eeaae477e4b50f5d14016a2fe4
Artiru <gignt0646@mail.ru>
2025-11-23 18:39:34 +0300

    Remove port

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,7 +4,7 @@ console.log(process.env.PROTOCOL)
 console.log(process.env.HOST_URL)
 
 export const api = axios.create({
-  baseURL: 'http://45.147.201.155:3000',
+  baseURL: 'http://45.147.201.155',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
